8c4ccd9750e2470940a564ba64d9c1dd571bcc96
ivanchikap <anton.ivanchik@gmail.com>
2019-08-30 18:25:44 +0300

    lesson15

---
+++ src/js/init.js
@@ -0,0 +1,43 @@
+(function () {
+    // .header-nav навигация
+    let nav = $('.menu__list');
+
+    // .menu-btn - кнопка меню
+    $('.menu__btn').click(function() {
+        if(nav.hasClass('menu__list--active')) {
+            nav.removeClass('menu__list--active');
+        }
+        else {
+            nav.addClass('menu__list--active');
+        }
+    });
+
+    $( "#products-release__tabs" ).tabs();
+
+    $( "#trending-products__carousel" ).owlCarousel({
+        dots:true,
+        margin: 30,
+        responsive:{
+            0: {
+                items: 1
+            },
+            720: {
+                items: 2
+            },
+            1200: {
+                items: 3
+            },
+            1440: {
+                items: 4
+            },
+        }
+    });
+
+}());
+
+
+
+
+// $(document).ready(function() {
+//
+// } );
